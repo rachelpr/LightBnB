33a52972469b42c6d6fc6e28fa1ebcfd284d3c08
Rachel PR <rplourder@gmail.com>
2022-04-07 22:18:20 -0300

    Add values for each table

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -15,3 +15,25 @@ INSERT INTO users (name, email, password)
   ),
   ('Leroy Hart','jaycereynolds@inbox.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'
   );
+
+INSERT INTO properties (title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
+VALUES ('Speed lamp', 'description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 93061, 6, 4, 8, 'Canada', '536 Namsub Highway', 'Sotboske', 'Quebec', '28142', true),
+('Blank corner', 'description', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 85234 , 6, 6, 7, 'Canada', '651 Nami Road', 'Bohbatev', 'Alberta', '83680', true),
+('Port out', 'description', 'https://images.pexels.com/photos/1475938/pexels-photo-1475938.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/1475938/pexels-photo-1475938.jpeg', 2358, 2, 8, 0, 'Canada', '1392 Gaza Junction', 'Upetafpuv', 'Nova Scotia', '81059', true),
+('Fun glad', 'description', 'https://images.pexels.com/photos/1172064/pexels-photo-1172064.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/1172064/pexels-photo-1172064.jpeg', 34291, 6, 6, 4, 'Canada', '169 Nuwug Circle', 'Vutgapha', 'Newfoundland And Labrador', '00159', true),
+('Shine twenty', 'description', 'https://images.pexels.com/photos/2076739/pexels-photo-2076739.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2076739/pexels-photo-2076739.jpeg', 13644, 1, 7, 8, 'Canada', '340 Dokto Park', 'Upfufa', 'Nova Scotia', '29045', true);
+
+
+INSERT INTO reservations (start_date, end_date, property_id, guest_id)
+  VALUES ('2018-09-11', '2018-09-26', 5, 3),
+  ('2021-10-01', '2021-10-14', 1, 3),
+  ('2019-01-04', '2019-02-01', 2, 1),
+  ('2014-10-21', '2014-10-21', 4, 5),
+  ('2016-07-17', '2016-08-01', 3, 4);
+
+INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
+  VALUES (2, 5, 2, 3, 'messages'),
+  (1, 4, 1, 3, 'messages'),
+  (8, 1, 2, 1, 'messages'),
+  (3, 5, 5, 4, 'messages'),
+  (4, 3, 4, 5, 'messages');
