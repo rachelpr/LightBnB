51066a500b92ef30198ebaa351befd8aef411d35
Rachel PR <rplourder@gmail.com>
2022-04-19 21:50:52 -0300

    Refactor database connection to indexk.js

---
+++ LightBnB_WebApp/server/database/index.js
@@ -0,0 +1,19 @@
+// Setup connection to postgres using set information in seperate file from routes
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "vagrant",
+  password: "123",
+  host: "localhost",
+  database: "lightbnb",
+});
+
+module.exports = {
+  async query(text, params) {
+    const start = Date.now();
+    const res = await pool.query(text, params);
+    const duration = Date.now() - start;
+    console.log("Expected query", { text, duration, rows: res.rows });
+    return res;
+  },
+};
